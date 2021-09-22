f2d230feebff558e5ddaf6d272c2e051713c7d20
Tamara Glushchenko <gluschenkotoma@ukr.net>
2021-09-22 14:32:14 +0300

    fetch

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,14 +1,18 @@
-
 //   Для HTTP-запросов используй публичный Pixabay API. Зарегистрируйся и получи ключ.
 const API_KEY = `22755596-bd5bc4dc11dbf7c870dedd292`;
 
 const BASE_URL = `https://pixabay.com/api/`;
-let searchQuery = ``,
-
+let searchQuery = ``;
 
 // URL-строка запроса:
 // https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ
 
-const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=12&key=${API_KEY}`
+let url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=12&key=${API_KEY}`;
 
-fetch(url).then(response=>console.log(response))
+fetch(url)
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  });
