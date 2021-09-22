2168c9ea77dba05a4878ef5a2bdf850ba2fc031f
Tamara Glushchenko <gluschenkotoma@ukr.net>
2021-09-22 12:02:56 +0300

    refs

---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+refs = {
+  searchForm: document.getElementById('search-form'),
+  picturesList: document.querySelector('.gallery'),
+  loadMoreBtn: document.getElementById('loadMore'),
+};
+console.log(refs.searchForm);
