3a7ae50507b946c432d14b9c32fc2f2b45fde6d9
Tamara Glushchenko <gluschenkotoma@ukr.net>
2021-09-22 14:03:06 +0300

    error

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,1 +1,14 @@
-import refs from './refs';
+
+//   Для HTTP-запросов используй публичный Pixabay API. Зарегистрируйся и получи ключ.
+const API_KEY = `22755596-bd5bc4dc11dbf7c870dedd292`;
+
+const BASE_URL = `https://pixabay.com/api/`;
+let searchQuery = ``,
+
+
+// URL-строка запроса:
+// https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ
+
+const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=12&key=${API_KEY}`
+
+fetch(url).then(response=>console.log(response))
